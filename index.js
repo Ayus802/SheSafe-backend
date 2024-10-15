63d40789fb74be31126765930bf1b60f0b01d95c
Ayush Kumar Gupta <guptayush47@gmail.com>
2024-10-16 00:30:55 +0530

    fix: cors

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const  router  = require('./routes/auth-route')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials: true,
+    optionsSuccessStatus: 200
+}))
 
 app.use(express.json())
 
